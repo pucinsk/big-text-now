8a195b35e4493ab43389ca203d8eeabc6d663d7c
Jokūbas Pučinskas <j.pucinskas@gmail.com>
2025-05-27 15:16:45 +0300

    Add favicon

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Bun dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+    in
+    {
+      devShells.aarch64-darwin.default = pkgs.mkShell {
+        packages = with pkgs; [
+          git
+          bun
+        ];
+      };
+    };
+}
